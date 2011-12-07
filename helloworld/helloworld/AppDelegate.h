e53c821df0bae503117127e93998562303a28da0
Philip Zhao <pzhao5@wisc.edu>
2011-12-07 12:40:34 -0600

    Testing!

---
+++ helloworld/helloworld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  helloworld
+//
+//  Created by Philip Zhao on 12/7/11.
+//  Copyright (c) 2011 University of Wisconsin-Madison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
