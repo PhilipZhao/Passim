be22776c8745858251f97338355c8840fd470b50
Philip Zhao <pzhao5@wisc.edu>
2011-12-07 12:43:01 -0600

    Test!!!

---
+++ helloworld/helloworld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  helloworld
+//
+//  Created by Philip Zhao on 12/7/11.
+//  Copyright (c) 2011 University of Wisconsin-Madison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ helloworld/helloworld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by Philip Zhao on 12/7/11.
+//  Copyright (c) 2011 University of Wisconsin-Madison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
