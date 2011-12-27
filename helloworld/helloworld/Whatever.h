0eade56b13b917c3ccbf8e9eb62bf22b1d251aa4
Borui Wang <bwang29@wisc.edu>
2011-12-27 15:43:21 -0600

    msg

---
+++ helloworld/helloworld/Whatever.h
@@ -0,0 +1,13 @@
+//
+//  Whatever.h
+//  helloworld
+//
+//  Created by Borui Wang on 12/27/11.
+//  Copyright (c) 2011 University of Wisconsin-Madison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Whatever : NSObject
+
+@end
---
+++ helloworld/helloworld/Whatever.m
@@ -0,0 +1,13 @@
+//
+//  Whatever.m
+//  helloworld
+//
+//  Created by Borui Wang on 12/27/11.
+//  Copyright (c) 2011 University of Wisconsin-Madison. All rights reserved.
+//
+
+#import "Whatever.h"
+
+@implementation Whatever
+
+@end
